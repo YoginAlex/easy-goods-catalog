c24d37d5223b57c6102121de75b787fc2b7549ad
Alexey Zakharenko <yogin.alex@gmail.com>
2014-06-07 15:02:01 +0400

    hide/show actions to admin

--- goods/admin.py
+++ goods/admin.py
@@ -6,11 +6,21 @@ admin.site.register(Color)
 admin.site.register(Size)
 admin.site.register(Type)
 
+def hide_from_site(modeladmin, request, queryset):
+    queryset.update(show=False)
+hide_from_site.short_description = u'Скрыть с сайта'
+
+def show_on_site(modeladmin, request, queryset):
+    queryset.update(show=True)
+show_on_site.short_description = u'Показать на сайте'
+
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('image_thumb',)
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'image_thumb', 'types', 'quantity', 'cost', 'show')
+    actions = [hide_from_site, show_on_site]
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Item, ItemAdmin)
+
