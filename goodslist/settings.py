58ba92f3a82a429d245f4be540612d984740b7cf
Alexey Zakharenko <yogin.alex@gmail.com>
2014-06-07 12:31:33 +0400

    gitignore

--- goodslist/settings.py
+++ goodslist/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for goodslist project.
 
@@ -68,15 +69,13 @@ DATABASES = {
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "templates/static"),
+)
